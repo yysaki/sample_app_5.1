63cf1f6ed30fe25b2897f932d45ec8210e2d38ca
yysaki <yysaki@mail.yysaki.com>
2019-04-12 23:47:44 +0900

    Fix to pass rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
+# rubocop:disable Style/ClassAndModuleChildren
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
+# rubocop:enable Style/ClassAndModuleChildren
