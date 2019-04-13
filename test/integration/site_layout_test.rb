d1719f7c4a8793be2317e093ec405c74f157c11e
yysaki <yysaki@mail.yysaki.com>
2019-04-13 18:04:09 +0900

    Rubocop autofix

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,22 +3,21 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
   end
 
-  test "access signup page" do
+  test 'access signup page' do
     get root_path
     assert_template 'static_pages/home'
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select 'title', full_title('Sign up')
   end
 end
