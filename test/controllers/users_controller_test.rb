824889bb66f9d2a93f3b529edbe2af883ad5d697
yysaki <yysaki@mail.yysaki.com>
2019-04-28 22:01:36 +0900

    Add User#admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,4 +30,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: 'foobar',
+              password_confirmation: 'foobar',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
+  end
+ end
