e49bf2cf5efe744c0ef49a0750157b9e7a36f1e6
yysaki <yysaki@mail.yysaki.com>
2019-04-28 18:01:37 +0900

    Add users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
