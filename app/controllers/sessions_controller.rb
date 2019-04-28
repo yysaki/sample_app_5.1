c6ad359db6ecd967f821de8997ab31ff8eaf7d69
yysaki <yysaki@mail.yysaki.com>
2019-04-28 09:25:38 +0900

    Add users#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+    else
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
