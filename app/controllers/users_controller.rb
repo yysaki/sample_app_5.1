03a1293a4d5df5738e9d294e96895e2a4e47332a
yysaki <yysaki@mail.yysaki.com>
2019-04-28 19:05:24 +0900

    Test successful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
