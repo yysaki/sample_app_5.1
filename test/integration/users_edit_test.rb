a2f37d52f3c11183de6fcc46b5da1d2a98d746af
yysaki <yysaki@mail.yysaki.com>
2019-04-28 21:00:30 +0900

    Check correct_user before edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test 'unsuccessful edit' do
