f8b7ae72d37de3f18d2dfcd2f87ca6b4c8b4cefb
yysaki <yysaki@mail.yysaki.com>
2019-04-29 18:04:45 +0900

    Verify picture

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,11 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    def picture_size
+      errors.add(:picture, 'should be less than 5MB') if picture.size > 5.megabytes
+    end
 end
