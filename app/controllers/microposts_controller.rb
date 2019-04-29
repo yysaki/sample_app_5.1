14c1664090ed9c24bfbf04e82f4dc4029d16a577
yysaki <yysaki@mail.yysaki.com>
2019-04-29 16:45:39 +0900

    Add microposts#{create,destroy}

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: %i[create destroy]
+
+  def create; end
+
+  def destroy; end
 end
