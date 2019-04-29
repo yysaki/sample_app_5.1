61d98e537def1c530388e49de7ac87030f5cf341
yysaki <yysaki@mail.yysaki.com>
2019-04-29 17:15:22 +0900

    Add feed partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,10 @@
 
 class StaticPagesController < ApplicationController
   def home
+    return unless logged_in?
+
     @micropost = current_user.microposts.build if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help; end
