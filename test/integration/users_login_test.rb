071331fd7bdb796d500d03791d7b6ef8662b119b
yysaki <yysaki@mail.yysaki.com>
2019-04-29 21:56:13 +0900

    Add users#following and users#followers

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -31,10 +31,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-
-    assert_select '#following'
-    assert_select '#followers'
-
     delete logout_path
     follow_redirect!
     assert_select 'a[href=?]', login_path
