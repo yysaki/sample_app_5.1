2619cf5a755609978430e0bcd222ceb18ac4202b
yysaki <yysaki@mail.yysaki.com>
2019-04-29 20:33:36 +0900

    Add following and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@
 
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name: 'Relationship',
+  has_many :active_relationships, inverse_of: :follower,
+                                  class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :passive_relationships, inverse_of: :followed,
+                                   class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -71,6 +78,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
