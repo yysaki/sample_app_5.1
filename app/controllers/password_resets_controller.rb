bde17a2eab6bd4ef2f770cfa74afd2f26bdae2bf
yysaki <yysaki@mail.yysaki.com>
2019-04-29 12:46:11 +0900

    Add password_resets#edit

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
   def new; end
 
   def create
@@ -17,4 +19,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless @user&.activated? &&
+             @user&.authenticated?(:reset, params[:id])
+        redirect_to root_url
+      end
+    end
 end
