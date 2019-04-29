f9c8c75f5781d17f2081d80011ae2e97dd04282d
yysaki <yysaki@mail.yysaki.com>
2019-04-29 17:44:45 +0900

    Add uploader

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'bcrypt', '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'carrierwave', '1.2.2'
 gem 'coffee-rails', '4.2.2'
 gem 'faker', '1.7.3'
 gem 'jbuilder', '2.7.0'
@@ -40,6 +41,7 @@ group :test do
 end
 
 group :production do
+  gem 'fog', '1.42'
   gem 'pg', '0.20.0'
 end
 
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -11,21 +11,23 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
-    assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: '' } }
-    end
+    post microposts_path, params: { micropost: { content: '' } }
     assert_select 'div#error_explanation'
 
     content = 'This micropost really ties the room together'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost:
+                                      { content: content,
+                                        picture: picture } }
     end
-    assert_redirected_to root_url
+    assert assigns(:micropost).picture?
     follow_redirect!
     assert_match content, response.body
 
-    assert_select 'a', text: 'delete'
+    assert_select 'a', 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
